e09150094d6d3c4f622f58152091395fbae523f4
henry117 <henry117@DESKTOP-6TNDAGD.home>
2019-05-15 20:52:47 +1000

    adding files

---
+++ MovieShop/src/java/uts/web/dao/DBConnector.java
@@ -0,0 +1,27 @@
+package uts.web.dao;
+
+import uts.web.dao.DB;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Initiate connection with the jdbc derby database and initialize the super class conn field
+ * 
+ * @author George
+ */
+public class DBConnector extends DB{
+
+    public DBConnector() throws ClassNotFoundException, SQLException {
+        Class.forName(driver);
+        conn = DriverManager.getConnection(URL+db, dbuser, dbpass);
+    }
+
+    public Connection openConnection(){
+        return this.conn;
+    }
+    
+    public void closeConnection() throws SQLException {
+        this.conn.close();
+    }
+}
