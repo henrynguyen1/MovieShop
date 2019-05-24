84dc2ff238344b174aaec65aae5eedc6fbcc7ab1
BenStevens <benjamin.g.stevens@gmail.com>
2019-05-24 23:06:25 +1000

    * Change DB name to MovieShopDB. * Initialise Connection object as null and move assignment to try-catch block.

--- MovieShop/src/java/uts/web/controller/DBConnector.java
+++ MovieShop/src/java/uts/web/controller/DBConnector.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /**
- * Initiate connection with the jdbc derby database and initialize
+ * Initiate connection with the JDBC derby database and initialize
  * the super class conn field.
  * Based on the "Preparing DAO Class" section of the following website:
  * https://www.codejava.net/coding/jsp-servlet-jdbc-mysql-create-read-update-delete-crud-example
@@ -20,36 +20,37 @@ public class DBConnector{
     private Connection conn;
     
     /**
-     * Default constructor for creating connections to MovieStoreDB.
+     * Default constructor for creating connections to MovieShopDB.
      */
     public DBConnector() {
-        URL = "jdbc:derby://localhost:1527/MovieStoreDB";
+        URL = "jdbc:derby://localhost:1527/MovieShopDB";
         DBUSER = "moviestore"; 
         DBPASS = "moviestorepassword";
         DRIVER = "org.apache.derby.jdbc.ClientDriver";
+        conn = null;
     }
 
     /**
-     * Open a connection to MovieStoreDB for CRUD operations.
-     * @return - connection to MovieStoreDB.
+     * Open a connection to MovieShopDB for CRUD operations.
+     * @return - connection to MovieShopDB.
      * @throws SQLException - No connections can be created if the DB is not
      *                        running.
      */
     protected Connection openConnection() throws SQLException {
         try {
             Class.forName(DRIVER);
+            conn = DriverManager.getConnection(URL, DBUSER, DBPASS);
         }
         catch (ClassNotFoundException cnfe) {
             //connection to DB could not be created.
             throw new SQLException(cnfe);
         }
-        conn = DriverManager.getConnection(URL, DBUSER, DBPASS);
         return this.conn;
     }
     
     /**
-     * Close the connection to MovieStoreDB when it is no longer needed.
-     * @throws SQLException - Connection to MovieStoreDB not found.
+     * Close the connection to MovieShopDB when it is no longer needed.
+     * @throws SQLException - Connection to MovieShopDB not found.
      */
     protected void closeConnection() throws SQLException {
         if (conn != null && !conn.isClosed()) {
